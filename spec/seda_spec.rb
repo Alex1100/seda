8a4d06dd4b8b4ffd6523abfa2961ac870d5cef45
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-11-21 11:54:13 -0800

    add tests for identity and first methods

--- spec/seda_spec.rb
+++ spec/seda_spec.rb
@@ -5,7 +5,20 @@ describe Seda do
     expect(Seda::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+
+  it "tests the identity method" do
+    test_result = Seda.identity('hi')
+
+    expect(test_result.class).to be(String)
+    expect(test_result).to eq('hi')
+  end
+
+
+  it "tests the first method" do
+    test_result_1 = Seda.first([1, 2, 3, 4], 5)
+    test_result_2 = Seda.first([1, 2, 3, 4], 2)
+
+    expect(test_result_1).to eq([1, 2, 3, 4])
+    expect(test_result_2).to eq([1, 2])
   end
 end
