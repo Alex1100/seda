792f8862ed6adf5b418b82fea72810859776c72d
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-11-20 18:36:06 -0800

    add defaults, index_of, identity, and first methods from underbar library

---
+++ lib/seda.rb
@@ -0,0 +1,75 @@
+require "seda/version"
+
+module Seda
+
+  def self.identity(value)
+    value
+  end
+
+  def self.first(arr, n_first_elements)
+    n_first_elements == nil ? arr[0] : arr.slice(0, n_first_elements)
+  end
+
+  def self.index_of(arr, target)
+    if(arr == nil)
+      return arr
+    end
+
+    result = -1
+
+    arr.each_with_index do |element, i|
+      if(element == target && result == -1)
+        result = i
+      end
+    end
+
+    result
+  end
+
+  def self.contains(collection, target)
+    collection.each do |element|
+      if element == target
+        return true
+      end
+    end
+    false
+  end
+
+
+  def self.defaults(*obj)
+    final_object = (eval(local_variables[0].to_s))[0]
+    iterables = (eval(local_variables[0].to_s))[1..-1]
+
+    if final_object != nil && iterables.class == Array
+      iterables.each do |element|
+        if element.class == Hash
+          p element.class
+          for k in element
+            if final_object[k[0].to_s] == nil
+              final_object[k[0].to_s] = k[1]
+            end
+          end
+        elsif element.class == Array
+          element.each do |item|
+            if final_object[item.to_s] == nil
+              final_object[item.to_s] = item
+            end
+          end
+        else
+          if final_object[element.to_s] == nil
+            final_object[element.to_s] = element
+          end
+        end
+      end
+    else
+      raise ArgumentError.new('Must pass in the destination object as first argument, and additional argument objects to extend values from')
+    end
+
+    return final_object
+  end
+
+
+
+
+
+end
---
+++ lib/seda/version.rb
@@ -0,0 +1,3 @@
+module Seda
+  VERSION = "0.1.0"
+end
